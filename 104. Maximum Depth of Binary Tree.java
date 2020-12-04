0951abde82f9e3eef13aa6cd35763b9c4dca11b0
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2020-12-04 23:28:43 +0530

    Update 104. Maximum Depth of Binary Tree.java

--- 104. Maximum Depth of Binary Tree.java
+++ 104. Maximum Depth of Binary Tree.java
@@ -1,4 +1,4 @@
-G/*iven the root of a binary tree, return its maximum depth.
+/*Given the root of a binary tree, return its maximum depth.
 
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
