edc276b754548e993c3974ca1d6f045c84840540
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2021-03-16 17:24:57 +0530

    Create 53. Maximum Subarray.java

---
+++ 53. Maximum Subarray.java
@@ -0,0 +1,60 @@
+/* Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum. */
+
+class Solution {
+    //Solution 1
+    /* Brute Force Algorithm:
+    O(n^3) solution.
+    First loop keeps a check of size of the sub array. 
+    Next nested loop keeps a track of the starting index.
+    Then put an if condition to check if that subarray can be made.
+    Then, the next for loop calculates the sum. 
+    Keep updating int ans = max(ans, sum). 
+    Outside all for loops, write return ans. 
+    Since it has 3 nested loops, hence time limit exceeded message is shown.
+    
+    Solution 2:
+    Adjust the solution 1 a little. Instead of first focussing 
+    on the number of elements in the subarray, focus on the starting index. 
+    The solution becomes O(n^2) as earlier, 
+    the same startinhg index was being accessed through different iterations.*/
+    public int maxSubArray(int[] nums) {
+        // int ans = Integer.MIN_VALUE;
+        // int n = nums.length;
+        // for(int numOfElements = 1; numOfElements <= n; numOfElements++) {
+        //     for(int startingIndex = 0; startingIndex < n; startingIndex++) {
+        //         if(startingIndex + numOfElements > n)
+        //             break;
+        //         int sum = 0;
+        //         for(int i = startingIndex; i < startingIndex + numOfElements; i ++) {
+        //             sum += nums[i];
+        //         }
+        //         ans = Math.max(sum,ans);
+        //     }
+        // }
+        // return ans;
+        
+        //Solution 2:
+        // int n = nums.length;
+        // int ans = Integer.MIN_VALUE;
+        // for(int startIndex = 0; startIndex < n; startIndex++) {
+        //     int sum = 0;
+        //     for(int numOfElements = 1; numOfElements <= n; numOfElements++) {
+        //         if(startIndex + numOfElements > n)
+        //             break;
+        //         sum += nums[startIndex + numOfElements - 1];
+        //         ans = Math.max(ans,sum);
+        //     }
+        // }
+        // return ans;
+        
+        //Solution 3:
+        //Kadane's algorithm!!
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+}
