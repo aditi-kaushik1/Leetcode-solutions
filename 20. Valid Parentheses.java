188a0a3581577232c6dc2d18b22f0180be1f33b5
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2021-03-16 13:52:31 +0530

    Update 20. Valid Parentheses.java

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -5,6 +5,7 @@ Open brackets must be closed in the correct order.*/
 
 class Solution {
     public boolean isValid(String s) {
+        //Solution 1
         //Algorithm:
         /*Corner case where first element is a closing bracket. 
         But in this solution, it is not necessary to specify that.
@@ -42,5 +43,56 @@ class Solution {
                 stack.push(c);
         }
         return stack.empty();
+        
+        //Solution 2
+        // Stack<Character> stack = new Stack<>();
+        // for(int i = 0; i < s.length(); i++) {
+        //     if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+        //         stack.push(s.charAt(i));
+        //     else if(s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(')
+        //         stack.pop();
+        //     else if(s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{')
+        //         stack.pop();
+        //     else if(s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[')
+        //         stack.pop();
+        //     else
+        //         return false;
+        // }
+        // return stack.isEmpty();
+        
+        //Solution 3
+        // Stack<Character>stack = new Stack<>();
+        // for(Character c : s.toCharArray()) {
+        //     if(c == '(')
+        //         stack.push(')');
+        //     else if(c == '{')
+        //         stack.push('}');
+        //     else if(c == '[')
+        //         stack.push(']');
+        //     else if(stack.isEmpty() || stack.pop() != c)
+        //         return false;
+        // }
+        // return stack.isEmpty();
+        
+        //Solution 4
+        //Check leetcode solution page
+//         Stack<Character> stack = new Stack<>();
+//         for(char c : s.toCharArray()) {
+//             if(mapping.containsKey(c)) {
+//                 char topElement = stack.isEmpty() ? '#' : stack.pop();
+//                 if(topElement != mapping.get(c))
+//                     return false;
+//             }
+//             else
+//              stack.push(c);
+//         }
+//         return stack.isEmpty();
+//     }
+//         private HashMap<Character, Character> mapping;
+//         public Solution() {
+//             mapping = new HashMap<Character, Character>();
+//             mapping.put(')','(');
+//             mapping.put('}','{');
+//             mapping.put(']','[');
     }
 }    
