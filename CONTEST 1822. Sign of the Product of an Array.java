b9f01895028c683383f4082bea85fbee27a8a44b
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2021-04-11 23:31:26 +0530

    Create CONTEST 1822. Sign of the Product of an Array.java

---
+++ CONTEST 1822. Sign of the Product of an Array.java
@@ -0,0 +1,28 @@
+/* There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+Return signFunc(product).
+
+Example 1:
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1 */
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        if(nums[0] != 0)
+            product = nums[0] / Math.abs(nums[0]);
+        if(nums[0] == 0)
+            return 0;
+        for(int i = 1; i< nums.length; i++) {
+            if(nums[i] == 0)
+                return 0;
+            product = product * (Math.abs(nums[i])/nums[i]);
+        }
+        return (product == 1) ? 1 : -1;
+    }
+}
