01e37b760a77af6e9639c4c43c5d3c9b581c34bd
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2021-04-22 22:00:51 +0530

    Update 84. Largest Rectangle in Histogram.java

--- 84. Largest Rectangle in Histogram.java
+++ 84. Largest Rectangle in Histogram.java
@@ -3,41 +3,73 @@ return the area of the largest rectangle in the histogram. */
 
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
+//         Stack<Integer> stack = new Stack<>();
+//         int n = heights.length;
+//         int[] ansLeft = new int[n];
+        
+//         for(int i = 0; i < n; i++) {
+//             while(!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+//                 stack.pop();
+//             if(stack.isEmpty())
+//                 ansLeft[i] = i;
+//             else
+//                 ansLeft[i] = i - stack.peek() - 1;
+//             stack.push(i);
+//         }
+        
+//         stack.clear();
+//         int[] ansRight = new int[n];
+        
+//         for(int i = n - 1; i >= 0; i--) {
+//             while(!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+//                 stack.pop();
+//             if(stack.isEmpty())
+//                 ansRight[i] = n - i - 1;
+//             else
+//                 ansRight[i] = stack.peek() - i - 1;
+//             stack.push(i);
+//         }
+        
+//         int[] sum = new int[n];
+        
+//         int finalAns = Integer.MIN_VALUE;
+        
+//         for(int i = 0; i < n; i++) {
+//             sum[i] = heights[i] * (ansLeft[i] + ansRight[i] + 1);
+//             finalAns = Math.max(sum[i],finalAns);
+//         }
+//         return finalAns
+        
+        //Solution 2: slightly different thinking used in calculations
         int n = heights.length;
         int[] ansLeft = new int[n];
-        
-        for(int i = 0; i < n; i++) {
-            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i])
-                stack.pop();
-            if(stack.isEmpty())
-                ansLeft[i] = i;
-            else
-                ansLeft[i] = i - stack.peek() - 1;
-            stack.push(i);
-        }
-        
-        stack.clear();
         int[] ansRight = new int[n];
-        
-        for(int i = n - 1; i >= 0; i--) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n; i++) {
             while(!stack.isEmpty() && heights[stack.peek()] >= heights[i])
                 stack.pop();
             if(stack.isEmpty())
-                ansRight[i] = n - i - 1;
+                ansLeft[i] = -1;
             else
-                ansRight[i] = stack.peek() - i - 1;
+                ansLeft[i] = stack.peek();
             stack.push(i);
         }
-        
-        int[] sum = new int[n];
-        
-        int finalAns = Integer.MIN_VALUE;
-        
-        for(int i = 0; i < n; i++) {
-            sum[i] = heights[i] * (ansLeft[i] + ansRight[i] + 1);
-            finalAns = Math.max(sum[i],finalAns);
+        stack.clear();
+        for(int i = n-1; i >= 0; i--) {
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            if(stack.isEmpty())
+                ansRight[i] = n;
+            else
+                ansRight[i] = stack.peek();
+            stack.push(i);
         }
-        return finalAns;
+        int maxArea = 0;
+        int[] area = new int[n];
+        for(int i = 0; i < n; i++) {
+            area[i] = heights[i]*(ansRight[i] - ansLeft[i] - 1);
+            maxArea = Math.max(maxArea,area[i]);
+        }
+        return maxArea;
     }
 }
