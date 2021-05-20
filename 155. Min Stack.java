ad0cec51efdd549f7f47407bc5a48627c3204579
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-05-20 11:53:03 +0530

    Update 155. Min Stack.java

--- 155. Min Stack.java
+++ 155. Min Stack.java
@@ -124,6 +124,38 @@ public class Node {
     }
 }
 
+//Solution 4
+//Instead of using a linked list, store an int pair in stack.
+
+// class MinStack {
+//     Stack<int[]> stack = new Stack<>();
+//     /** initialize your data structure here. */
+//     public MinStack() {
+        
+//     }
+    
+//     public void push(int val) {
+//         if(stack.isEmpty()) {
+//             stack.push(new int[]{val, val});
+//             return;
+//         }
+//         int x = stack.peek()[1];
+//         stack.push(new int[]{val, Math.min(x, val)});
+//     }
+    
+//     public void pop() {
+//         stack.pop();
+//     }
+    
+//     public int top() {
+//         return stack.peek()[0];
+//     }
+    
+//     public int getMin() {
+//         return stack.peek()[1];
+//     }
+// }
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
