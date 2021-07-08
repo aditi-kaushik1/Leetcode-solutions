3eccda7c0b429c8f72dd60a638d8c6f85cc58aef
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-07-08 11:14:23 +0530

    Update 14. Longest Common Prefix.java

--- 14. Longest Common Prefix.java
+++ 14. Longest Common Prefix.java
@@ -8,24 +8,39 @@ strs[i] consists of only lower-case English letters. */
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        /*This problem can be solved using divide and conquer as well as
-        binary search too, but they are not optimised 
-        because of extra space and time complexity. */
-        //Solution using Tries
+        //Use sorting
+        //We can sort the array in lexicographical order
+        //and then find the common prefix between 1st and last terms
+        //(2 most dissimilar terms).
         if(strs.length == 0)
             return "";
         if(strs.length == 1)
             return strs[0];
-        for(String s : strs)
-            insert(s);
-        Node current = root;
-        String ans = "";
+        Arrays.sort(strs);
+        int min = Math.min(strs[0].length(), strs[strs.length - 1].length());
         int i = 0;
-        while(true) {
-            if(current.children.isEmpty() || current.isEndOfWord || current.children.size() > 1)
-                return ans;
-            ans += strs[0].charAt(i);
-            current = current.children.get(strs[0].charAt(i++));
+        while(i < min && strs[0].charAt(i) == strs[strs.length - 1].charAt(i))
+            i++;
+        return strs[0].substring(0, i);
+        
+        /*This problem can be solved using divide and conquer as well as
+        binary search too, but they are not optimised 
+        because of extra space and time complexity. */
+        //Solution using Tries
+        // if(strs.length == 0)
+        //     return "";
+        // if(strs.length == 1)
+        //     return strs[0];
+        // for(String s : strs)
+        //     insert(s);
+        // Node current = root;
+        // String ans = "";
+        // int i = 0;
+        // while(true) {
+        //     if(current.children.isEmpty() || current.isEndOfWord || current.children.size() > 1)
+        //         return ans;
+        //     ans += strs[0].charAt(i);
+        //     current = current.children.get(strs[0].charAt(i++));
         }
         
         //Solution
@@ -67,7 +82,7 @@ class Solution {
 //             str += s1.charAt(i);
 //         }
 //         return str;
-    }
+//   }
     
     // public int min(String[] strs) {
     //     int x = strs[0].length();
@@ -83,25 +98,25 @@ class Solution {
     which has no children(end of word), or has more than 1 children
     (branching is not allowed). */
     
-    public class Node {
-        char value;
-        HashMap<Character, Node> children = new HashMap<>();
-        boolean isEndOfWord;
+//     public class Node {
+//         char value;
+//         HashMap<Character, Node> children = new HashMap<>();
+//         boolean isEndOfWord;
         
-        Node(char value) {
-            this.value = value;
-        }
-    }
+//         Node(char value) {
+//             this.value = value;
+//         }
+//     }
     
-    Node root = new Node(' ');
+//     Node root = new Node(' ');
     
-    public void insert(String word) {
-        Node current = root;
-        for(char ch : word.toCharArray()) {
-            if(!current.children.containsKey(ch))
-                current.children.put(ch, new Node(ch));
-            current = current.children.get(ch);
-        }
-        current.isEndOfWord = true;
-    }
+//     public void insert(String word) {
+//         Node current = root;
+//         for(char ch : word.toCharArray()) {
+//             if(!current.children.containsKey(ch))
+//                 current.children.put(ch, new Node(ch));
+//             current = current.children.get(ch);
+//         }
+//         current.isEndOfWord = true;
+//     }
 }
