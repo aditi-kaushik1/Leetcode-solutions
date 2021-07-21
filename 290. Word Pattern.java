6f12c49ac1e7f6edb4056a57132969005eb8686e
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-07-21 18:54:56 +0530

    Create 290. Word Pattern.java

---
+++ 290. Word Pattern.java
@@ -0,0 +1,77 @@
+/* Given a pattern and a string s, find if s follows the same pattern.
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+Example 4:
+Input: pattern = "abba", s = "dog dog dog dog"
+Output: false
+ 
+Constraints:
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lower-case English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space. */
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        //Solution 1
+        //Using HashMap
+        //containsValue() has time complexity O(n)
+        //Time complexity of solution 1 and 2 is same
+        
+        HashMap<Character, String> map = new HashMap<>();
+        String[]  strArr = s.split(" ");
+        
+        if(pattern.length() != strArr.length)
+            return false;
+        
+        for(int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            
+            if(map.containsKey(ch)) {
+                if(!map.get(ch).equals(strArr[i]))
+                    return false;
+            }
+            
+            else {
+                if(map.containsValue(strArr[i]))
+                    return false;
+                map.put(ch, strArr[i]);
+            }
+        }
+        return true;
+        
+        //Solution 2
+        //Using another helper method to check if the map contains the same value or not
+        
+//         HashMap<Character, String> map = new HashMap<>();
+//         String[]  strArr = s.split(" ");
+//         if(pattern.length() != strArr.length)
+//             return false;
+//         int i = 0;
+//         for(char ch : pattern.toCharArray()) {
+//           if(mapContains(map, strArr[i]) != '$' && mapContains(map, strArr[i]) != ch)
+//                 return false;
+//             if(map.containsKey(ch) && !map.get(ch).equals(strArr[i]))
+//                 return false;
+//             map.put(ch, strArr[i++]);
+//         }
+//         return true;
+//     }
+    
+//     public char mapContains(HashMap<Character, String> map, String s) {
+//         for(char ch : map.keySet())
+//             if(map.get(ch).equals(s))
+//                 return ch;
+//         return '$';
+    }
+}
