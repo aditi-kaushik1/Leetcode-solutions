91bb7f0a87f86a1486877af6bc29bc1f4554d8d7
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2022-10-11 12:03:00 +0530

    Update 1790. Check if One String Swap Can Make Strings Equal.java

--- 1790. Check if One String Swap Can Make Strings Equal.java
+++ 1790. Check if One String Swap Can Make Strings Equal.java
@@ -1,8 +1,8 @@
-/* You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+/* You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string 
+(not necessarily different) and swap the characters at these indices.
 
-Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
-
- 
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. 
+Otherwise, return false.
 
 Example 1:
 
