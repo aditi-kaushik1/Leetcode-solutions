f8b4d02cf19a4066eae5f262c6fd3777034c08ad
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2023-01-12 19:37:24 +0530

    Create 520. Detect Capital.java

---
+++ 520. Detect Capital.java
@@ -0,0 +1,29 @@
+/* We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+Example 1:
+
+Input: word = "USA"
+Output: true
+
+Example 2:
+Input: word = "FlaG"
+Output: false
+
+Constraints:
+
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters. */
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if((word.toLowerCase().equals(word)) || (word.toUpperCase().equals(word)) || (word.substring(1).toLowerCase().equals(word.substring(1))))
+            return true;
+        else
+            return false;
+    }
+}
